953d883fa61b16bb0268ffc49cde5b233370b736
rks14899 <116179673+rks14899@users.noreply.github.com>
2025-07-21 20:39:49 +0530

    Update Write.jsx

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -72,3 +72,22 @@ export default function Write() {
             className="writeInput"
             autoFocus={true}
             onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+            onChange={(e) => setDesc(e.target.value)}
+          ></textarea>
+        </div>
+
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
+}
